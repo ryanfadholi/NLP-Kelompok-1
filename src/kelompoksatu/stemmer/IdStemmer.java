1bb65880caf583b37567b7b853724cc70f71ffc2
Muhammad Ryan Fadholi <rynfdhl@gmail.com>
2017-11-16 12:23:34 +0700

    Added rule 23/24

--- src/kelompoksatu/stemmer/IdStemmer.java
+++ src/kelompoksatu/stemmer/IdStemmer.java
@@ -15,11 +15,24 @@ import java.util.stream.Stream;
  */
 public class IdStemmer {
     
-    private final static char[] CONSONANTS = {'b','c','d','f','g','h','j','k',
+     private final static char[] CONSONANTS = {'b','c','d','f','g','h','j','k',
         'l','m','n','p','q','r','s','t','v','w','x','y','z'};
     
     private final static char[] VOWELS = {'a','i','u','e','o'};
 
+    private final static char[] GHQ = {'g','h','q'};
+
+    private final static String[] INFLECTION_SUFFIXES = {
+        "lah", "kah", "ku", "mu", "nya", "tah", "pun"
+    };
+    
+    private final static String[] INFLECTION_SUFFIXES_PARTICLES = {
+        "lah", "kah", "tah", "pun"
+    };
+    
+    private final static String[] INFLECTION_SUFFIXES_POSSESIVE_PRONOUNS = {
+       "ku", "mu", "nya"
+    };
     private final static PrefixSuffixPair[] FORBIDDEN_PREFIX_SUFFIX_PAIRS = {
         new PrefixSuffixPair("be", "i"),
         new PrefixSuffixPair("di", "an"),
@@ -31,58 +44,9 @@ public class IdStemmer {
         new PrefixSuffixPair("te", "an")
     };
     
-//Definisi Rule 21 pada array setara dengan:
-//    
-//    Rule r = new Rule("21", 
-//            new RuleMatcher() {
-//                @Override
-//                public boolean match(String word) {
-//                    if(word.startsWith("per")){
-//                        if(isVowel(word.charAt(3))){
-//                            return true;
-//                        }
-//                    }
-//                    return false;
-//                }},
-//            new RuleStemmer() {
-//                @Override
-//                public String process(String word) {
-//                    ArrayList<String> possibleBaseWords = new ArrayList<>();
-//                    String initialStem = word.replace("per","");
-//                
-//                    possibleBaseWords.add(initialStem);
-//                    possibleBaseWords.add("r" + initialStem);
-//                
-//                    return BaseWordsManager.getFirstMatch(possibleBaseWords);
-//                }
-//            });
-    
     private final static Rule[] RULES = {
+        
         //rule 21
-            new Rule("21", 
-            new RuleMatcher() {
-                @Override
-                public boolean match(String word) 
-                {//peraturan
-                    if(word.startsWith("per")){
-                        if(isVowel(word.charAt(3))){
-                            return true;
-                        }
-                    }
-                    return false;
-                }},
-            new RuleStemmer() {
-                @Override
-                public String process(String word) {
-                    ArrayList<String> possibleBaseWords = new ArrayList<>();
-                    String initialStem = word.replaceFirst("per","");
-          
-                    possibleBaseWords.add(initialStem);
-                    possibleBaseWords.add("r" + initialStem);
-                
-                    return BaseWordsManager.getFirstMatch(possibleBaseWords, word);
-                }
-            }),
         new Rule("21", 
                 (w) -> w.startsWith("per") && isVowel(w, 3),
                 (w) -> {
@@ -95,9 +59,13 @@ public class IdStemmer {
                 return BaseWordsManager.getFirstMatch(possibleBaseWords,
                         w);
                 }),
-        
+        //rule 23
+        new Rule("23/24",
+                (w) -> w.startsWith("per"),
+                (w) -> w.replaceFirst("per", "")),
+        //rule 28
         new Rule("28", 
-                (w) -> w.startsWith("pen") && isVowel(w.charAt(3)),
+                (w) -> w.startsWith("pen") && isVowel(w, 3),
                 (w) -> {
                 ArrayList<String> possibleBaseWords = new ArrayList<>();
                 String initialStem = w.replaceFirst("pen","");
@@ -108,15 +76,16 @@ public class IdStemmer {
                 return BaseWordsManager.getFirstMatch(possibleBaseWords,
                         w);
                 }),
-        //rule 23, 24
-        //........................
         //rule 30
         new Rule("30", 
-                (w) -> w.startsWith("peng") && isVowel(w.charAt(4)),
+                (w) -> w.startsWith("peng") && isVowel(w, 4),
                 (w) -> {
                 ArrayList<String> possibleBaseWords = new ArrayList<>();
                 String initialStem = w.replaceFirst("peng","");
-                
+
+                if(initialStem.startsWith("e")){
+                    initialStem = initialStem.replaceFirst("e","");
+                }
                 possibleBaseWords.add(initialStem);
                 possibleBaseWords.add("k" + initialStem);
                 
@@ -140,12 +109,12 @@ public class IdStemmer {
                 })
     };
     
-    private static boolean isConsonant(char c){
+ private static boolean isConsonant(char c){
         Stream<Character> consonantStream = IntStream.range(0, CONSONANTS.length).mapToObj(i -> CONSONANTS[i]);
         return consonantStream.anyMatch((s) -> s == c);
     }
     
-     private static boolean isConsonant(String word, int pos){
+    private static boolean isConsonant(String word, int pos){
         //Apabila posisi diluar panjang string, return false
         if(word.length() < pos+1){
             return false;
@@ -167,8 +136,47 @@ public class IdStemmer {
         
         return isVowel(word.charAt(pos));
     }
+    
+    private static boolean isGHQ(char c){
+        Stream<Character> GHQStream = IntStream.range(0, GHQ.length).mapToObj(i -> GHQ[i]);
+        return GHQStream.anyMatch((s) -> s == c);
+    }
+    
+     private static boolean isGHQ(String word, int pos){
+        //Apabila posisi diluar panjang string, return false
+        if(word.length() < pos+1){
+            return false;
+        }
+        
+        return isGHQ(word.charAt(pos));
+    }
+     
+     private static boolean isParticle(String inflectionSuffix){
+         for(String particle : INFLECTION_SUFFIXES_PARTICLES){
+             if(inflectionSuffix.equals(particle)){
+                 return true;
+             }
+         }
+         return false;
+     }
+    
     private static String preprocess(String word){
         return word.trim().toLowerCase();
+    }
+    
+    private static String cutSuffix(String word, String suffix){
+       return word.substring(0, word.length() - suffix.length());
+    }
+    
+    private static String cutPossesivePronouns(String word){
+        
+        for(String possesive_pronoun : INFLECTION_SUFFIXES_POSSESIVE_PRONOUNS){
+            if(word.endsWith(possesive_pronoun)){
+                return cutSuffix(word, possesive_pronoun);
+            }
+        }
+        
+        return word;
     }
     
      private static boolean containsForbiddenPair(String word){
@@ -199,26 +207,117 @@ public class IdStemmer {
         }
         
         String processedQuery = originalQuery;
-        String tempQuery = processedQuery;
+        
+        //-----------------------------------------------
+        //LANGKAH 2 
+        processedQuery = applyStep2(processedQuery);
+        //C4 apakah kata sudah berbentuk kata dasar 
+        System.out.println("Langkah 2: " + processedQuery);
+        
+        if(BaseWordsManager.isBaseWord(processedQuery)){
+            return processedQuery;
+        }
+        
+        //-----------------------------------------------
+        //LANGKAH 3
+        processedQuery = applyStep3(processedQuery);
+        //Cek apakah kata sudah berbentuk kata dasar 
+        if(BaseWordsManager.isBaseWord(processedQuery)){
+            return processedQuery;
+        }
+        
+        //-----------------------------------------------
+        //LANGKAH 4
+        processedQuery = applyStep4(processedQuery);
+        //Cek apakah kata sudah berbentuk kata dasar
+        if(BaseWordsManager.isBaseWord(processedQuery)){
+            return processedQuery;
+        }
+        
+        //Jika tidak ada di kamus, asumsikan query adalah kata dasar.
+        return originalQuery;
+    }
+    
+    /*
+    Aplikasi aturan kedua
+    */
+    private static String applyStep2(String word){
+       
+        String result = word;
+        String cut_suffix = null;
+        
+        for(String suffix : INFLECTION_SUFFIXES){
+            if(word.endsWith(suffix)){
+                result = cutSuffix(result, suffix);
+                cut_suffix = suffix;
+            }
+        }
+        
+        //kalau tidak null, artinya ada suffix yang dipotong.
+        if(cut_suffix != null){
+            if(isParticle(cut_suffix)){
+                result = cutPossesivePronouns(result);
+            }
+        }
+        
+        return result;
+    }
+    
+     /*
+    Aplikasi aturan ketiga
+    */
+    private static String applyStep3(String word){
+        String result;
+        
+        if(word.endsWith("i")){
+            result = cutSuffix(word,"i");
+            
+            if(BaseWordsManager.isBaseWord(applyStep4(result))){
+                return result;
+            } 
+        }
+        
+        if(word.endsWith("an")){
+            result = cutSuffix(word,"an");
+            if(BaseWordsManager.isBaseWord(applyStep4(result))){
+                return result;
+            }
+            
+            if(result.endsWith("k")) {
+                result = cutSuffix(result, "k");
+                
+                if(BaseWordsManager.isBaseWord(applyStep4(result))){
+                    return result;
+                }
+            }
+            
+        }
+        
+        return word;
+    }
+    
+    private static String applyStep4(String word){
+        String result = word;
+        String temp = result;
         
         int ruleCount = 0;
         String previousRule = "";
         String currentRule;
         
         while(ruleCount < 3){
-            
             currentRule = previousRule;
+            
             //Cek jika kata mengandung pasangan prefix-suffix yang dilarang
-            if(containsForbiddenPair(originalQuery)){
-                return originalQuery;
+            if(containsForbiddenPair(word)){
+                return word;
             }
             
             for(Rule rule : RULES){
-                if(rule.match(processedQuery)){
+                if(rule.match(result)){
                     currentRule = rule.definition();
                     System.out.println(rule.definition() + "applied.");
-                    tempQuery = rule.apply(processedQuery);
-                    if(BaseWordsManager.isBaseWord(tempQuery)){
+                    temp = rule.apply(result);
+                    if(BaseWordsManager.isBaseWord(temp)){
                         break;
                     }
                 }
@@ -238,23 +337,19 @@ public class IdStemmer {
                 break;
             } else {
                 previousRule = currentRule;
-                processedQuery = tempQuery;
+                result = temp;
             }
             
             //Cek apakah kata sudah berbentuk kata dasar
-            if(BaseWordsManager.isBaseWord(originalQuery)){
+            if(BaseWordsManager.isBaseWord(result)){
                 break;
             }
             
             ruleCount++;
         }
         
-        System.out.println("Iterated " + ruleCount + " times.");
+        System.out.println("Rule 4 iterates " + ruleCount + " times.");
         
-        if(BaseWordsManager.isBaseWord(processedQuery)){
-            return processedQuery;
-        }
-        
-        return originalQuery;
+        return result;
     }
 }
